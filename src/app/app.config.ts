ecaeb2fa5e5f7995c05c2efd4fd9c084338d64e4
Deyson Venté <deiandresvent@hotmail.com>
2025-01-25 10:12:47 -0500

    Update app configuration to use experimental zoneless change detection and add animations and Toastr providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,18 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideExperimentalZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideExperimentalZonelessChangeDetection(),
+    provideAnimations(),
+    provideToastr(),
+    provideRouter(routes),
+  ],
 };
