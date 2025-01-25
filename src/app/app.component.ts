3a81e9fede7d41eddb92d113e7feb00122bc279a
Deyson Venté <deiandresvent@hotmail.com>
2025-01-25 10:13:41 -0500

    -Refactor app component to implement currency conversion form - Removed placeholder content and integrated header and footer components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+} from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Moneda } from './enum/Moneda';
+import { ConversorService } from './services/conversor.service';
+import { Convertir } from './interfaces/IConvertir';
+import { FooterComponent, HeaderComponent } from './components';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, FooterComponent, CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'appConversorDivisa';
+  private readonly toast = inject(ToastrService);
+  private readonly conversorService = inject(ConversorService);
+  constructor() {}
+
+  public monedas: Moneda[] = [Moneda.COP, Moneda.USD, Moneda.EUR];
+  //*Campos del formulario
+  public monedaOrigen: string = '';
+  public monedaDestino: string = '';
+  public cantidad: number = 0;
+  public result = computed(() => {
+    return this.conversorService.result();
+  });
+
+  handleSubmit() {
+    if (this.monedaDestino === this.monedaOrigen) {
+      this.toast.info('Escoge unas monedas diferentes.');
+      return;
+    }
+    if (
+      this.monedaDestino.length === 0 ||
+      this.monedaOrigen.length === 0 ||
+      this.cantidad === 0
+    ) {
+      this.toast.error('Debes llenar todos los campos');
+    } else {
+      const model: Convertir = {
+        monedaDestino: this.monedaDestino as Moneda,
+        monedaOrigen: this.monedaOrigen as Moneda,
+        cantidad: this.cantidad,
+      };
+      this.conversorService.convertir(model);
+      this.toast.success(`Resultado: ${this.result()}`);
+    }
+  }
 }
