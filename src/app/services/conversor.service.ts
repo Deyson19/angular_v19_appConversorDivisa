c1570021a2aa8e100804ac624e48dfaccecaf630
Deyson Venté <deiandresvent@hotmail.com>
2025-01-30 10:37:53 -0500

    Refactor ConversorService to use external API for conversion rates
    Updated the ConversorService to fetch conversion rates from an external API instead of using hardcoded rates. This change includes:
    - Adding a method to fetch the exchange rate from the API.
    - Modifying the `convertir` method to use the fetched exchange rate for conversion.
    - Storing conversion history in `conversionsList`.
    - Removing the old conversion methods and hardcoded rates.

--- src/app/services/conversor.service.ts
+++ src/app/services/conversor.service.ts
@@ -1,68 +1,43 @@
 import { Injectable, signal } from '@angular/core';
-import { Convertir } from '../interfaces/IConvertir';
-import { Moneda } from '../enum/Moneda';
-import { PrecioMoneda } from '../helpers/precioMoneda';
+import { Moneda } from '@Enums/Moneda';
+import { Convertir, Exchange } from '@Interfaces/index';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ConversorService {
   public result = signal(0);
+  private readonly exchangeUrl: string =
+    'https://api.exchangerate-api.com/v4/latest';
+
+  public conversionsList: Convertir[] = [];
   constructor() {}
 
-  public convertir(model: Convertir) {
-    switch (model.monedaOrigen) {
-      case Moneda.COP:
-        this.convertFromCOP(model);
-        break;
-      case Moneda.USD:
-        this.convertFromUSD(model);
-        break;
-      case Moneda.EUR:
-        this.convertFromEUR(model);
-        break;
-      default:
-        console.log('No se ha seleccionado una moneda');
-        break;
-    }
-  }
-
-  private convertFromCOP(model: Convertir): void {
-    const conversionRates = {
-      [Moneda.USD]: PrecioMoneda.CopToUsd,
-      [Moneda.EUR]: PrecioMoneda.CopToEuro,
-    };
-    //*realizar conversion
-    this.buildConversion(model, conversionRates);
-  }
-
-  private convertFromUSD(model: Convertir): void {
-    const conversionRates = {
-      [Moneda.COP]: PrecioMoneda.UsdToCop,
-      [Moneda.EUR]: PrecioMoneda.UsdToEuro,
-    };
-    //*realizar conversion
-    this.buildConversion(model, conversionRates);
-  }
-
-  private convertFromEUR(model: Convertir): void {
-    const conversionRates = {
-      [Moneda.COP]: PrecioMoneda.EuroToCop,
-      [Moneda.USD]: PrecioMoneda.EuroToUsd,
-    };
-    //*realizar conversion
-    this.buildConversion(model, conversionRates);
-  }
-  private buildConversion(
-    model: Convertir,
-    rates: { [key: string]: () => number }
-  ) {
-    const conversion = rates[model.monedaDestino];
-    if (conversion) {
-      const result = model.cantidad * conversion();
-      this.result.set(result);
+  public async convertir(model: Convertir) {
+    const price = await this.GetPrice(model.monedaOrigen, model.monedaDestino);
+    let result = 0;
+    if (price > model.cantidad) {
+      result = price / model.cantidad;
     } else {
-      console.error('No hay conversion disponible');
+      result = model.cantidad / price;
     }
+    const conversion: Convertir = {
+      cantidad: model.cantidad,
+      monedaOrigen: model.monedaOrigen,
+      monedaDestino: model.monedaDestino,
+      total: result,
+    };
+    this.conversionsList.push(conversion);
+    this.result.set(result);
+  }
+
+  private async GetPrice(monedaOrigen: Moneda, monedaDestino: Moneda) {
+    const request = await fetch(`${this.exchangeUrl}/${monedaDestino}`);
+    if (request.status === 200) {
+      const data = (await request.json()) as Exchange;
+      const price = data.rates![monedaOrigen];
+      return price;
+    }
+    return 0;
   }
 }
