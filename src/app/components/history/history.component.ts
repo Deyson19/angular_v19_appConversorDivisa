accd0e1901dfa7e4b479ddee707347240ff645ef
Deyson Venté <deiandresvent@hotmail.com>
2025-01-30 10:35:32 -0500

    Update import path for Convertir interface
    Modify the import statement to use the new path from the @Interfaces/index module to ensure consistency and maintainability.

--- src/app/components/history/history.component.ts
+++ src/app/components/history/history.component.ts
@@ -1,5 +1,5 @@
 import { Component, input } from '@angular/core';
-import { Convertir } from '../../interfaces/IConvertir';
+import { Convertir } from '@Interfaces/index';
 import { CommonModule } from '@angular/common';
 
 @Component({
