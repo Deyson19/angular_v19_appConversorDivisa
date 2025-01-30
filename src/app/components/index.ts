7e192c5e82999f394e58199d6ab9af608e8b7879
Deyson Venté <deiandresvent@hotmail.com>
2025-01-29 20:11:12 -0500

    Export history component

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,2 +1,3 @@
+export { HistoryComponent } from './history/history.component';
 export { FooterComponent } from './footer/footer.component';
 export { HeaderComponent } from './header/header.component';
